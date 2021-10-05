d767410128735f25a21253fce76dbc3b293a78d8
Daxmatic <daxomatic@gmail.com>
2021-10-05 14:02:38 +0200

    Initial Commit

---
+++ NasaMarsRovers/NasaMarsRoversApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaMarsRoversApp.swift
+//  NasaMarsRovers
+//
+//  Created by Dax Hoes on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NasaMarsRoversApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
