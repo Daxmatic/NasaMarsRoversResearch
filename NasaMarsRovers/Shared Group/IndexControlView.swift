ca37817bd16d891a153083a3645815af140873a2
Daxmatic <daxomatic@gmail.com>
2021-10-05 14:51:01 +0200

    Working indexScroll thingy, now the nice part, combine as messenger or if to steep for me, i could always use @environmentObject

--- NasaMarsRovers/Shared Group/IndexControlView.swift
+++ NasaMarsRovers/Shared Group/IndexControlView.swift
@@ -7,9 +7,56 @@
 
 import SwiftUI
 
+struct ImageStore: Identifiable {
+    let name: String
+    let id: Int
+}
+
 struct IndexControlView: View {
+    let imageNames = [
+        ImageStore(name: "a.circle.fill", id: 0),
+        ImageStore(name: "b.circle.fill", id: 1),
+        ImageStore(name: "c.circle.fill", id: 2),
+        ImageStore(name: "d.circle.fill", id: 3),
+        ImageStore(name: "e.circle.fill", id: 4),
+        ImageStore(name: "f.circle.fill", id: 5),
+        ImageStore(name: "g.circle.fill", id: 6),
+        ImageStore(name: "h.circle.fill", id: 7),
+        ImageStore(name: "i.circle.fill", id: 8),
+        ImageStore(name: "j.circle.fill", id: 9),
+        ImageStore(name: "k.circle.fill", id: 10),
+        ImageStore(name: "l.circle.fill", id: 11),
+        ImageStore(name: "m.circle.fill", id: 12),
+        ImageStore(name: "n.circle.fill", id: 13),
+        ImageStore(name: "o.circle.fill", id: 14),
+        ImageStore(name: "p.circle.fill", id: 15),
+        ImageStore(name: "q.circle.fill", id: 16),
+    ]
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollViewReader { value in
+            HStack {
+                Button("Go to letter Q") {
+                    value.scrollTo(16)
+                }
+
+                Button("Go back to A") {
+                    value.scrollTo(0)
+                }
+            }
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(imageNames) { image in
+                        Image(systemName: image.name)
+                            .id(image.id)
+                            .font(.largeTitle)
+                            .foregroundColor(Color.yellow)
+                            .frame(width: 90, height: 90)
+                            .background(Color.blue)
+                            .padding()
+                    }
+                }
+            }
+        }
     }
 }
 
